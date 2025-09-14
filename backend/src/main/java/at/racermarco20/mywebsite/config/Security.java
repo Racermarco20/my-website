46ac586c58712e76d41918e9b7c0796dd67ea1d8
4AHITS Braun Marco <old-account.marco.braun@stud.it.htl-hl.ac.at>
2025-09-14 17:39:08 +0200

    chore(security): edited security config

--- backend/src/main/java/at/racermarco20/mywebsite/config/Security.java
+++ backend/src/main/java/at/racermarco20/mywebsite/config/Security.java
@@ -2,9 +2,9 @@ package at.racermarco20.mywebsite.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 
@@ -24,8 +24,7 @@ public class Security {
                         .anyRequest().authenticated()
                 )
 
-                .httpBasic(httpBasic -> {
-                });
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
